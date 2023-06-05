12a8d8568987131d9bd9d07040c505e34c88174e
jenny-hub90 <jumbledjenny19@gmail.com>
2023-06-05 11:08:05 +0545

    completed the ui for sign in

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
